20667d6a428f7fd237188ed67acb08c5cae2fc50
korshunovamm <korshunova.mm@phystech.edu>
2024-05-19 20:28:45 +0300

    added

--- launch.sh
+++ launch.sh
@@ -9,13 +9,3 @@ docker build -t fetcher:latest -f Dockerfile.fetcher .
 
 kubectl create -f app-deployment.yaml
 kubectl create -f fetcher-deployment.yaml
-
-minikube tunnel
-
-# Load Docker images into Kubernetes
-#minikube image load fetcher:latest --image-pull-policy=Never
-#minikube image load app:latest --image-pull-policy=Never
-#
-## Apply Kubernetes manifests
-#kubectl apply -f app-deployment.yaml
-#kubectl apply -f fetcher-deployment.yaml

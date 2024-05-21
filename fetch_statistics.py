cc4e1d2ee8ce5904251952ee22dcd7f9d70b4226
korshunovamm <korshunova.mm@phystech.edu>
2024-05-21 19:30:05 +0300

    added

--- fetch_statistics.py
+++ fetch_statistics.py
@@ -2,13 +2,12 @@ import requests
 import time
 
 url = "http://app-service:5000/statistics"
-while True:
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            with open('statistics.txt', 'a') as f:
-                f.write(f"{time.ctime()}: {data['time_request_count']}\n")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    time.sleep(5)
+try:
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        with open('statistics.txt', 'a') as f:
+            f.write(f"{time.ctime()}: {data['time_request_count']}\n")
+except Exception as e:
+    print(f"An error occurred: {e}")
+time.sleep(5)
